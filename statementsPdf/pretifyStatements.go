8fb0e5595fd1f09eb93ce46673467ce50f1f968c
markel3266 <markel3266@gmail.com>
2019-11-14 22:05:23 +0200

    added error handling to trimCsv and prettyCsvArr functions and fix rounding of float value in convertCoins function

--- statementsPdf/pretifyStatements.go
+++ statementsPdf/pretifyStatements.go
@@ -1,6 +1,7 @@
 package statementsPdf
 
 import (
+	"errors"
 	"strconv"
 	"time"
 )
@@ -17,7 +18,7 @@ var headers = []string{"Time", "Description", "Amount", "Balance"}
 func convertCoins(coins string) string {
 	amountCoins, _ := strconv.ParseFloat(coins, 64)
 	amount := amountCoins / 100
-	return strconv.FormatFloat(amount, 'f', 1, 64)
+	return strconv.FormatFloat(amount, 'f', 2, 64)
 }
 
 func convertTimestamp(timestamp string) string {
@@ -26,24 +27,30 @@ func convertTimestamp(timestamp string) string {
 	return timeUnix.Format("2006-01-02 15:04:05")
 }
 
-func prettyCsvArr(statements [][]string) [][]string {
-	statements = trimCsv(statements)
+func prettyCsvArr(statements [][]string) ([][]string, error) {
+	statements, err := trimCsv(statements)
+	if err != nil {
+		return nil, err
+	}
 	for row := 0; row < len(statements); row++ {
 		statements[row][dateColumn] = convertTimestamp(statements[row][dateColumn])
 		statements[row][amountColumn] = convertCoins(statements[row][amountColumn])
 		statements[row][balanceColumn] = convertCoins(statements[row][balanceColumn])
 	}
-	return statements
+	return statements, nil
 }
 
-func trimCsv(statements [][]string) [][]string {
+func trimCsv(statements [][]string) ([][]string, error) {
 	var trimmedStatements [][]string
 	var newRow []string
 	for row := 0; row < len(statements); row++ {
+		if len(statements[row]) != 11 {
+			return nil, errors.New("Count of column is incorrect ")
+		}
 		newRow := append(newRow, statements[row][1:3]...)
 		newRow = append(newRow, statements[row][5:6]...)
 		newRow = append(newRow, statements[row][10:11]...)
 		trimmedStatements = append(trimmedStatements, newRow)
 	}
-	return trimmedStatements
+	return trimmedStatements, nil
 }
